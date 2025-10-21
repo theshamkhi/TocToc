9476112b581f871dba5797bafb48a01493683002
Mohammed SHAMKHI <theshamkhi1@gmail.com>
2025-10-21 11:35:01 +0100

    Created Livreur @Entity

--- src/main/java/com/toctoc/entity/Livreur.java
+++ src/main/java/com/toctoc/entity/Livreur.java
@@ -1,4 +1,42 @@
 package com.toctoc.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "livreurs")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Livreur {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Le nom est obligatoire")
+    @Column(nullable = false)
+    private String nom;
+
+    @NotBlank(message = "Le prénom est obligatoire")
+    @Column(nullable = false)
+    private String prenom;
+
+    @NotBlank(message = "Le véhicule est obligatoire")
+    private String vehicule;
+
+    @NotBlank(message = "Le téléphone est obligatoire")
+    @Pattern(regexp = "^[0-9]{10}$", message = "Format téléphone invalide")
+    @Column(unique = true)
+    private String telephone;
+
+    @OneToMany(mappedBy = "livreur", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @Builder.Default
+    private List<Colis> colisList = new ArrayList<>();
 }
