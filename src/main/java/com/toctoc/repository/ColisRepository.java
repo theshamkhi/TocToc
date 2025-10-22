51a0f7fc65225aaf8ec9c55dd1f1261c71007c5e
Mohammed SHAMKHI <theshamkhi1@gmail.com>
2025-10-22 16:30:34 +0100

    Added necessary repositories extending from JpaRepository

--- src/main/java/com/toctoc/repository/ColisRepository.java
+++ src/main/java/com/toctoc/repository/ColisRepository.java
@@ -1,4 +1,12 @@
 package com.toctoc.repository;
 
-public class ColisRepository {
+import com.toctoc.entity.Colis;
+import com.toctoc.entity.StatutColis;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ColisRepository extends JpaRepository<Colis, Long> {
+    List<Colis> findByLivreurId(Long livreurId);
+    List<Colis> findByStatut(StatutColis statut);
 }
--- src/main/java/com/toctoc/repository/LivreurRepository.java
+++ src/main/java/com/toctoc/repository/LivreurRepository.java
@@ -1,4 +1,11 @@
 package com.toctoc.repository;
 
-public class LivreurRepository {
+import com.toctoc.entity.Livreur;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface LivreurRepository extends JpaRepository<Livreur, Long> {
+    Optional<Livreur> findByTelephone(String telephone);
+    boolean existsByTelephone(String telephone);
 }
